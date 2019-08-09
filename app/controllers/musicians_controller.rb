5b85026d68dcb61908125ad931c0a5c522c282c9
Austin Arts <austinjarts@gmail.com>
2019-08-09 16:41:39 +0000

    quiz2

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -4,4 +4,19 @@ class MusiciansController < ApplicationController
     @musicians = Musician.all
   end 
 
+  def new
+    @musician = Musician.new
+  end 
+
+  def create
+    Musician.create(musician_params)
+    redirect_to root_path
+  end 
+
+
+  private
+
+  def musician_params
+    params.require(:musician).permit(:name, :description, :hometown)
+  end 
 end
